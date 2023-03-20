e0358477e43327b42e1ccc8585dd87797da34e67
m3trik@live.com <m3trik@live.com>
2023-03-20 15:04:02 -0400

    Update update_wheel.cmd

--- update_wheel.cmd
+++ update_wheel.cmd
@@ -10,7 +10,6 @@ set "name=mayatk"
 ::
 set "dir=%CLOUD%\Code\_scripts\%name%"
 
-
 ::delete previous build directories:
 rem ECHO %cd%
 echo Attempting to remove "%dir%/build" ..
@@ -19,10 +18,9 @@ echo Attempting to remove "%dir%/dist" ..
 rmdir /s /q "%dir%/dist"
 ECHO/
 
-
-python -c "from pythontk import File; File.incVersion('%dir%/%name%/__init__.py')"
-echo %name% package version number incremented.
-
+:: Get and update the version number
+for /f "delims=" %%i in ('python -c "from pythontk import File; ver = File.updateVersion(r'%dir%/%name%/__init__.py'); print(ver)"') do set ver=%%i
+echo %name% package version number incremented to %ver%.
 
 :: upload the package wheel to pipy
 cd /d %dir%
@@ -31,7 +29,22 @@ python -m pip install --user --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 :: 
 python -m pip install --user --upgrade twine
-twine upload dist/* 
+
+:: Set your PyPI username and password (use an API token as the password)
+set /p TWINE_USERNAME=Enter your username:
+set /p TWINE_PASSWORD=Enter your password:
+
+:: Attempt to upload using twine and check for errors
+twine upload --username %TWINE_USERNAME% --password %TWINE_PASSWORD% dist/* 2> upload_errors.txt
+if %errorlevel% neq 0 (
+    echo Twine upload failed. Reverting the version number.
+    python -c "from pythontk import File; ver = File.updateVersion(r'%dir%/%name%/__init__.py', 'decrement')"
+) else (
+    echo Twine upload successful.
+)
+
+:: Remove temporary file
+del upload_errors.txt
 
 
 PAUSE
