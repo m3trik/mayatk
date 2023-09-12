2be7bd59a46835b2592fe6bc8a789879a756cfe0
m3trik@live.com <m3trik@live.com>
2023-09-12 10:39:40 -0400

    0.7.8
    - xform_utils.match_scale:  fixed parameter order.

--- mayatk/mat_utils/__init__.py
+++ mayatk/mat_utils/__init__.py
@@ -2,13 +2,6 @@
 # coding=utf-8
 
 from mayatk.mat_utils._mat_utils import MatUtils
-import pythontk as ptk
-
-ptk.append_path(r"O:\Cloud\Code\_scripts\mayatk\mayatk\mat_utils", recursive=True)
-try:
-    import MaterialX
-except ImportError as error:
-    print(__file__, error)
 
 # -----------------------------------------------------------------------------
 
