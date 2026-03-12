576e161165a906feb09312c1decd90f08f64196f
m3trik@live.com <m3trik@live.com>
2026-03-11 22:48:48 -0400

    Update dependencies & bump version to 0.10.26 [skip ci]

--- mayatk/__init__.py
+++ mayatk/__init__.py
@@ -4,7 +4,7 @@ from pythontk.core_utils.module_resolver import bootstrap_package
 
 
 __package__ = "mayatk"
-__version__ = "0.10.25"
+__version__ = "0.10.26"
 
 """Dynamic Attribute Resolver for Module-based Packages
 
